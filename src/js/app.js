895901b1f8202b09815c530118dc297cf537d894
zsline <sergei.dereka@gmail.com>
2022-10-25 12:14:32 +0300

    2

--- src/js/app.js
+++ src/js/app.js
@@ -13,18 +13,18 @@ const swiper = new Swiper();
 const url = 'https://zsline.github.io/iliinsky/src/js/products.json';
 const request = new XMLHttpRequest();
 request.open('GET', url);
-request.responseType = 'text';
+request.responseType = 'json';
 request.send();
 request.onload = function() {
     const productCategory = request.response;
-    const dataProducts = JSON.parse(productCategory);
-    categoryName(dataProducts);
-    // console.log(dataProducts['market']);
+    // const dataProducts = JSON.parse(productCategory);
+    categoryName(productCategory);
+    // console.log(productCategory);
   }
 function categoryName (jsObj){
     let category = jsObj;
     for (let i = 0; i < category.length; i++){
-console.log(i);
+console.log([i]);
     }
 }
 
