48b2a548f0d7bf2e9a3b3b8a732150fd232bee21
zsline <sergei.dereka@gmail.com>
2022-10-25 15:14:05 +0300

    json5

--- src/js/app.js
+++ src/js/app.js
@@ -19,15 +19,12 @@ request.onload = function() {
     const productCategory = request.response;
     const jsonProduct = JSON.parse(productCategory);
     console.log(jsonProduct);
-    // console.log(jsonProduct.market['name-menu']);
     categoryName(jsonProduct);
   }
 function categoryName (jsObj){
     let category = jsObj;
-    console.dir(category.cooking);
     for (let i = 0; i < category.length; i++){
-        let a = i['name-menu'];
-        console.log(a);
+        // console.log(i);
     }
 }
 
